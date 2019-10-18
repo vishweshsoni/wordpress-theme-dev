b87fe09231c82de3319867c4b3065de71631c004
Vishwesh Soni <vishweshsoni@gmail.com>
2019-10-19 01:46:51 +0530

    added html

--- themes/designfly/header.php
+++ themes/designfly/header.php
@@ -6,21 +6,29 @@
  *
  *
  * @package designfly
- */ 
+ */
 
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes();?> >
-	
+
 	<head>
 		<meta charset="<?php bloginfo('charset');?>">
 		<meta name="viewport" content="width=device-width,initial-scale=1">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
-		<?php if(is_singular() && pings_open(get_queried_object())): ?>
-		<link rel="pingback" href="<?php bloginfo( 'pingback_url' );?>">
-		<?php endif; ?>
-		<?php wp_head(); ?>
+		<?php if (is_singular() && pings_open(get_queried_object())): ?>
+		<link rel="pingback" href="<?php bloginfo('pingback_url');?>">
+		<?php endif;?>
+		<?php wp_head();?>
 	</head>
 	<body <?php body_class();?>>
+	<div class="navigation-bar">
+			<div id="navigation-container">
+				<img src="<?php header_image();?>">
+				<ul>
+					<li></li>
+				</ul>
+			</div>
+	<div>
 
 
